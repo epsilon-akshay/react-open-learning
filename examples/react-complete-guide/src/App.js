e064e08660b8194488c7fbb370cd42a5f64fc25e
akshay.nambiar <akshay.nambiar@go-jek.com>
2020-09-15 11:30:42 +0530

    removed unused code

---
+++ examples/react-complete-guide/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        This is basic react app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
